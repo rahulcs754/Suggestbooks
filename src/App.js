76bf7100975ec5380363a2e1837b414a92260c90
rahul shukla <rahul.shukla754@gmail.com>
2021-08-12 22:34:16 +0530

    Docs : add input button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  var [data, setdata] = useState("");
+
+  function searchSuggestHandler(e) {
+    console.log(e.target.value);
+    var basePath =
+      "https://kitsu.io/api/edgeanime?filter[categories]=adventure";
+    fetch(basePath)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Welcome Suggestion Website</h1>
+      <p>
+        {" "}
+        you are searching movie like wanted or series then you just type name in
+        input box
+      </p>
+      <input onChange={searchSuggestHandler} />
+      <h2>Output Below</h2>
     </div>
   );
 }
